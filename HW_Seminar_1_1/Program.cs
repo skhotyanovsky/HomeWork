4a6e01b6b1ac8bfccb958a06a830945b08eaa184
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-02-25 16:12:29 +0200

    Task1

---
+++ HW_Seminar_1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Input integer");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA < numberB && numberB < numberC)
+{     
+    Console.WriteLine("Max integer " + numberC); 
+}
+else if (numberA < numberB && numberC < numberB)
+{
+    Console.WriteLine("Max integer " + numberB);
+}
+else 
+{
+     Console.WriteLine("Max integer " + numberA);
+}
