3b7a4c73709027d4950eb7077f4f7ae3c8f706d8
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-02-26 11:04:19 +0200

    Task3

---
+++ HW_Seminar_1_2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Input integer A and B");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = Convert.ToInt32(Console.ReadLine());
+if(numberA < numberB) 
+{
+     Console.WriteLine("Max is " + numberB + " and Min is " + numberA);
+} 
+else
+{
+     Console.WriteLine("Max is " + numberA + " and Min is " + numberB);  
+}
---
+++ HW_Seminar_1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Input integer");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 2;
+result = numberA / numberB;
+num = result % 10;
+if(result = 0) 
+{
+    Console.WriteLine("Integer chetnoye");
+}
+else
+{
+    Console.WriteLine("Ineger nechetnoye");
+}
