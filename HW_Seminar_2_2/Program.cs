228c65c1ac5e535352df04f32b6194bbece51f2d
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-03-02 16:16:55 +0200

    Task2

---
+++ HW_Seminar_2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input integer");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99)
+{
+    while(number > 999)
+    number /= 10;
+    Console.WriteLine("Your result is " + number % 10);
+}
+else
+{
+    Console.WriteLine("Wrong number");
+}
