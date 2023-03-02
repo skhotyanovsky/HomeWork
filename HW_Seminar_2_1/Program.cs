5a97e744914e9e2562d2640d8db627411be24cd6
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-03-02 14:45:48 +0200

    Task1

---
+++ HW_Seminar_2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Input integer");
+int A = Convert.ToInt32(Console.ReadLine());
+int B = A / 10;
+int C = B % 10;
+if (100<= A && A <=999)
+{
+    Console.WriteLine("Second number of integer is " + C);
+}
+else
+{
+    Console.WriteLine("Wrong integer");
+}
