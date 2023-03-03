2c0c144b3f445ebcaf4005475e51ef09178ee8d2
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-03-03 13:35:00 +0200

    Task3

---
+++ HW_Seminar_2_3/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Input integer of a week day");
+int number = Convert.ToInt32(Console.ReadLine());
+switch(number)
+{
+    case 1:
+    Console.WriteLine("no");
+    break;
+    case 2:
+    Console.WriteLine("no");
+    break;
+    case 3:
+    Console.WriteLine("no");
+    break;
+    case 4:
+    Console.WriteLine("no");
+    break;
+    case 5:
+    Console.WriteLine("no");
+    break;
+    case 6:
+    Console.WriteLine("YES");
+    break;
+    case 7:
+    Console.WriteLine("YES");
+    break;
+    default:
+    Console.WriteLine("Wrong number");
+    break;
+}
+
+
+    
