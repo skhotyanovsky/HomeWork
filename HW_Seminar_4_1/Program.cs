1c9b8bf37b52ba9e0c36588513b7a41727de2377
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-03-07 11:32:57 +0200

    Task1

---
+++ HW_Seminar_4_1/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Input integer");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input degree");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("a^b=" + Math.Pow(a,b));
