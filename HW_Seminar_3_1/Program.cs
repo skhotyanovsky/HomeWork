906937a27d787038888ef499c8a575b146b69489
skhotyanovsky <s.khotyanovsky@gmail.com>
2023-03-14 10:42:02 +0200

    Task1

---
+++ HW_Seminar_3_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Input integer");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void CheckingNumber(int a)
+{
+    string number = a.ToString();
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"Integer: {number} - palindrom");
+    }
+    else Console.WriteLine($"Integer: {number} is not palindrom");
+}
+if (number > 9999 && number < 100000)
+{
+    CheckingNumber(number);
+}
